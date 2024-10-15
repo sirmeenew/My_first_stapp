10762e8018113139e9cc19a6b024a2ff075156ad
sirmeenew <69440879+sirmeenew@users.noreply.github.com>
2024-10-15 11:47:27 +0900

    Upload_subpage2

---
+++ pages/02_subpage2.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.title("서브페이지 2")
+st.write("")
+st.write("# 마크다운 H1 : st.write()")
+st.write("### 마크다운 H3 : st.write()")
+st.write("- 목록")
+st.write('')
+
+st.title("제목 : st.title()")
+st.header(" 헤더 : st.header()")
+st.subheader("서브헤더 : st.subheader()")
+st.text("본문 텍스트 : st.text()")
+st.write("")
