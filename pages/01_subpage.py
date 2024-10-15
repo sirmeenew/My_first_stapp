6c6ba643afc122bca925791d28980740b314a9fd
sirmeenew <69440879+sirmeenew@users.noreply.github.com>
2024-10-15 11:39:53 +0900

    Create 01_subpage.py

---
+++ pages/01_subpage.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.title("여기는 서브페이지 1 입니다")
+st.write("")
+st.write("# 마크다운 H1 : st.write()")
+st.write("### 마크다운 H3 : st.write()")
+st.write("- 목록")
+st.write('')
+
+st.title("제목 : st.title()")
+st.header(" 헤더 : st.header()")
+st.subheader("서브헤더 : st.subheader()")
+st.text("본문 텍스트 : st.text()")
+st.write("")
